2e0be614236f913b4cca81595528659017d5ff09
Roshan Kajulkar <roshankajulkar@gmail.com>
2024-01-29 18:24:00 +0530

    join game api

---
+++ client/src/pages/api/monopoly/join-game.ts
@@ -0,0 +1,94 @@
+import { gql } from "@apollo/client";
+import { apolloClient } from "@/apollo";
+import { NextApiResponse } from "next";
+import { authenticate } from "../middleware/auth";
+import { ExtendedNextApiRequest } from "@/types/jwt";
+
+const getGameDetailsQuery = gql`
+  query GetUser($gameId: uuid!) {
+    monopoly_game_by_pk(id: $gameId) {
+      settings
+      admin
+      player_sequence
+      game_state
+    }
+  }
+`;
+
+const addParticipantQuery = gql`
+  mutation addParticipantQuery(
+    $gameId: uuid!
+    $playerId: uuid!
+    $playerSequence: [uuid!]!
+  ) {
+    insert_monopoly_game_participant_one(
+      object: { game_id: $gameId, player_id: $playerId }
+    ) {
+      id
+    }
+
+    update_monopoly_game_by_pk(
+      pk_columns: { id: $gameId }
+      _set: { player_sequence: $playerSequence }
+    ) {
+      id
+    }
+  }
+`;
+
+const handler = async (req: ExtendedNextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    return res
+      .status(405)
+      .json({ error: "Method Not Allowed. Expecting POST." });
+  }
+
+  const userInfo = req.user;
+
+  if (!userInfo) {
+    return res.status(400).json({ error: "missing information in token." });
+  }
+
+  const userId = userInfo.id;
+  const { gameId } = req.body;
+
+  if (!gameId) {
+    return res.status(400).json({ error: "missing gameId." });
+  }
+
+  try {
+    const { data } = await apolloClient.query({
+      query: getGameDetailsQuery,
+      variables: { gameId },
+    });
+
+    if (data.monopoly_game_by_pk === null) {
+      return res.status(400).json({ error: "Invalid gameId." });
+    }
+
+    if (data.monopoly_game_by_pk.player_sequence.includes(userId)) {
+      return res.status(200).json({ data });
+    }
+
+    if (
+      data.monopoly_game_by_pk.player_sequence.length <
+      data.monopoly_game_by_pk.settings.maxPlayers
+    ) {
+      const addParticipantGameResult = await apolloClient.mutate({
+        mutation: addParticipantQuery,
+        variables: {
+          gameId,
+          playerId: userId,
+          playerSequence: [...data.monopoly_game_by_pk.player_sequence, userId],
+        },
+      });
+      return res.json(addParticipantGameResult);
+    } else {
+      return res.json({ access: "View ONLY" });
+    }
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export default authenticate(handler);
--- client/src/pages/monopoly/room/[id].tsx
+++ client/src/pages/monopoly/room/[id].tsx
@@ -2,8 +2,42 @@ import { ProtectRoute } from "@/components/protected-route";
 import styles from "./style.module.scss";
 import MonopolyBoard from "@/components/monopoly/board";
 import { useAuth } from "@/context/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Room = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    const joinGame = async (gameId: string) => {
+      try {
+        const res = await axios.post(
+          "/api/monopoly/join-game",
+          {
+            gameId,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+
+        console.log(res);
+      } catch (error) {
+        console.error("Error fetching game details:", error);
+      }
+    };
+
+    if (id) {
+      const gameId = Array.isArray(id) ? id[0] : id;
+      joinGame(gameId);
+    }
+  }, [id]);
+
   return (
     <ProtectRoute>
       <div className={styles.container}>
